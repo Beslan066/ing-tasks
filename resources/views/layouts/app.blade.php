992841e95ee03094023e4339791cf46ed73cbcbd
Beslan066 <64842276+Beslan066@users.noreply.github.com>
2026-05-16 11:54:16 +0300

    Добавил счетчик яндекс метрики

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2146,7 +2146,19 @@ media-src https://meet.jit.si https:;
     overlay.addEventListener('click', closeSidebar);
 </script>
 
+<!-- Yandex.Metrika counter -->
+<script type="text/javascript">
+    (function(m,e,t,r,i,k,a){
+        m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+        m[i].l=1*new Date();
+        for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+        k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+    })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=109251601', 'ym');
 
+    ym(109251601, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", referrer: document.referrer, url: location.href, accurateTrackBounce:true, trackLinks:true});
+</script>
+<noscript><div><img src="https://mc.yandex.ru/watch/109251601" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+<!-- /Yandex.Metrika counter -->
 @stack('scripts')
 
 </body>
