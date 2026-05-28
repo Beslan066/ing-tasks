326ba3f366a1b5fd2b3ce380c38fbbc5b9a9f2b9
Beslan066 <64842276+Beslan066@users.noreply.github.com>
2026-05-28 14:23:09 +0300

    Поправил вебхук для изменения подписки после оплаты 2

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,6 +15,11 @@ return Application::configure(basePath: dirname(__DIR__))
         App\Providers\EventServiceProvider::class,
     ])
     ->withMiddleware(function (Middleware $middleware): void {
+
+        $middleware->validateCsrfTokens(except: [
+            '/licence/payment/webhook',
+            'licence/payment/webhook',
+        ]);
         // Только редирект для неавторизованных
         $middleware->redirectGuestsTo('/login');
 
