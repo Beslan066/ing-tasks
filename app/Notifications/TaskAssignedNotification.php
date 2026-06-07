871cb44db038cb1a260da8c359746ccf343edcb7
Beslan066 <64842276+Beslan066@users.noreply.github.com>
2026-06-07 14:47:37 +0300

    Фикс ошибки неправильного роута в письме о задаче

--- app/Notifications/TaskAssignedNotification.php
+++ app/Notifications/TaskAssignedNotification.php
@@ -23,7 +23,12 @@ class TaskAssignedNotification extends Notification implements ShouldQueue
 
     public function toMail(object $notifiable): MailMessage
     {
-        $taskUrl = route('tasks.view', $this->task->id);
+        // Отправляем ссылку на страницу со списком задач с параметром open_task
+        // Это откроет страницу /team/tasks и автоматически покажет модальное окно с задачей
+        $taskUrl = route('tasks.index') . '?open_task=' . $this->task->id;
+
+        // Или если у вас есть отдельный роут для team/tasks
+        // $taskUrl = url('/team/tasks?open_task=' . $this->task->id);
 
         return (new MailMessage)
             ->subject('Вам назначена новая задача: ' . $this->task->name)
